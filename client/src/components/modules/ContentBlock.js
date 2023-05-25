5b8d70a03d1caab440565dcd1b93ca389f04d55e
Chun-Hei Lam <chun.lam18@imperial.ac.uk>
2023-05-25 19:33:30 +0100

    h1 animation styling

--- client/src/components/modules/ContentBlock.js
+++ client/src/components/modules/ContentBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 import "../../utilities.css";
 import "./ContentBlock.css";
@@ -13,10 +13,31 @@ import "./ContentBlock.css";
  */
 
 const ContentBlock = ( {title, children} ) => {
-  
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+          setIsIntersecting(entry.isIntersecting);
+      }, 
+      { rootMargin: "-150px" }    
+    );
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [isIntersecting])
+
+  useEffect(() => {
+    if (isIntersecting) {
+        ref.current.querySelectorAll("h1").forEach((el) => {
+            el.classList.add("contentblock-title")
+        })
+    }
+  }, [isIntersecting])
+
   return (
-      <div className="contentblock">
-        <h1 className="contentblock-title">{title}</h1>
+      <div className="contentblock" ref={ref}>
+        <h1>{title}</h1>
         {children}
       </div>
     );
